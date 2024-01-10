56602ad7eef3f632e1f0db380528900be204e727
Hirenkumar <hirenkumarht40@gmail.com>
2024-01-10 13:08:04 +0530

    updated

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const moviesData = require('./moviesData.json');
+const express = require("express");
+const cors = require("cors");
+const moviesData = require("./moviesData.json");
 
 const app = express();
+//
 const port = 5000;
 
 app.use(cors());
 
-app.get('/api/movies', (req, res) => {
+app.get("/api/movies", (req, res) => {
   res.json(moviesData.movies);
 });
 
-app.get('/api/movies/:id', (req, res) => {
+app.get("/api/movies/:id", (req, res) => {
   const movieId = parseInt(req.params.id);
 
   const movie = moviesData.movies.find((m) => m.id === movieId);
 
   if (!movie) {
-    return res.status(404).json({ error: 'Movie not found' });
+    return res.status(404).json({ error: "Movie not found" });
   }
 
   res.json(movie);
